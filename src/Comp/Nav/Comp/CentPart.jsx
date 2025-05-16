c4dc7d9a0a44e83cfdd1be542b604e6fd867e81b
Aidimys <mklirill939@gmail.com>
2025-05-16 19:14:26 +0300

    Nav: CenterPart and RightPart

--- src/Comp/Nav/Comp/CentPart.jsx
+++ src/Comp/Nav/Comp/CentPart.jsx
@@ -1,4 +1,15 @@
+import { FaSearch } from "react-icons/fa";
+import "./CentPart.css";
 const CentPart = () => {
-  return <></>;
+  return (
+    <div className='CentPart'>
+      <form>
+        <input className='SearchInp' placeholder='Search' />
+        <button className='SearchBtn'>
+          <FaSearch />
+        </button>
+      </form>
+    </div>
+  );
 };
 export default CentPart;
--- src/Comp/Nav/Comp/RightPart.jsx
+++ src/Comp/Nav/Comp/RightPart.jsx
@@ -1,4 +1,22 @@
+import { FaTh, FaBell, FaVideo } from "react-icons/fa";
+import emptyUser from "./images/emptyUser.png";
+import "./RightPart.css";
 const RightPart = () => {
-  return <></>;
+  return (
+    <div className='RightPart'>
+      <button className='RghtBtn'>
+        <FaVideo />
+      </button>
+      <button className='RghtBtn'>
+        <FaTh />
+      </button>
+      <button className='RghtBtn'>
+        <FaBell />
+      </button>
+      <button className='UserMenuBtn'>
+        <img src={emptyUser} alt='user' className='userImg' />
+      </button>
+    </div>
+  );
 };
 export default RightPart;
