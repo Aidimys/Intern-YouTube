507791cae499c919f80300e303133941ea6f6c01
Aidimys <mklirill939@gmail.com>
2025-06-19 18:54:05 +0300

    Author page pixel perfect, adaptive pixel perfect for main page and author page

--- src/Comp/AuthorContent/AuthorContent.jsx
+++ src/Comp/AuthorContent/AuthorContent.jsx
@@ -10,7 +10,10 @@ const AuthorContent = () => {
   const thirdSectionVideos = videoData.slice(9, 15);
   return (
     <div className='AuthorContent'>
-      <img src={backImg} alt='back' className='backImg' />
+      <div className='BackImg-div'>
+        <img src={backImg} alt='back' className='backImg' />
+      </div>
+
       <section className='AuthorSection'>
         <div className='AutInfPose'>
           <img src={author} alt='author' className='AuthorImg2' />
@@ -23,7 +26,7 @@ const AuthorContent = () => {
           <button className='RightBtn'>
             <FaBell />
           </button>
-          <button className='SubscribeBtn'>Subscribe 245k</button>
+          <button className='SubscribeBtn2'>Subscribe 245k</button>
         </div>
       </section>
       <section className='MainSection'>
@@ -69,56 +72,53 @@ const AuthorContent = () => {
                 </button>
               </li>
             </ul>
-            <div>
-              <div className='VideoDiv'>
-                <div className='imgDiv'>
-                  <img src={bigImg} alt='preview' className='bigImg' />
-                  <div className='timeIcon'>7:36</div>
-                </div>
-                <div className='DescriptionDiv'>
-                  <h3>Some text of video name</h3>
-                  <p className='Description'>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
-                  </p>
-                  <div className='shortInf2'>
-                    <p className='vidInfo2'>11k views</p>
-                    <p className='vidInfo2'>·</p>
-                    <p className='vidInfo2'>6 month ago</p>
-                  </div>
-                </div>
+          </nav>
+          <div className='VideoDiv'>
+            <div className='imgDiv'>
+              <img src={bigImg} alt='preview' className='bigImg' />
+              <div className='timeIcon'>7:36</div>
+            </div>
+            <div className='DescriptionDiv'>
+              <h3>Some text of video name</h3>
+              <p className='Description'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type.
+              </p>
+              <div className='shortInf2'>
+                <p className='vidInfo2'>11k views</p>
+                <p className='vidInfo2'>·</p>
+                <p className='vidInfo2'>6 month ago</p>
               </div>
             </div>
-          </nav>
+          </div>
         </div>
         <div className='RecChan'>
           <p>Recommended channels</p>
           <ul className='RecChanList'>
             <li>
               <a href='youtuber'>
-                <img src={author} alt='author' className='AuthorImg2' />
+                <img src={author} alt='author' className='AuthorImg' />
                 <h4>Youtuber1</h4>
               </a>
             </li>
             <li>
               <a href='youtuber'>
-                <img src={author} alt='author' className='AuthorImg2' />
+                <img src={author} alt='author' className='AuthorImg' />
                 <h4>Youtuber1</h4>
               </a>
             </li>
             <li>
               <a href='youtuber'>
-                <img src={author} alt='author' className='AuthorImg2' />
+                <img src={author} alt='author' className='AuthorImg' />
                 <h4>Youtuber1</h4>
               </a>
             </li>
           </ul>
         </div>
       </section>
-      <section>
+      <section id='author-vid-section'>
         <div className='SectionInfo'>
           <h3>Food & Drink videos</h3>
           <div className='CtrlBtns'>
